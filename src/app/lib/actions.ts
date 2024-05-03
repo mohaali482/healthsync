29eea846cd50da20a4eddaa3a9e1a3eadc65a2a2
Mohammed Ali <mohaali482@gmail.com>
2024-05-03 10:05:50 +0300

    refactor: format the files using prettier

--- src/app/lib/actions.ts
+++ src/app/lib/actions.ts
@@ -1,27 +1,24 @@
-"use server";
+'use server';
 
-import { signIn, signOut } from "@/lib/auth";
-import { AuthError } from "next-auth";
-import { revalidatePath } from "next/cache";
+import { signIn, signOut } from '@/lib/auth';
+import { AuthError } from 'next-auth';
 
 export async function authenticate(formData: FormData) {
-    try {
-        await signIn("credentials", formData);
-        revalidatePath("/login");
-        return null;
-    } catch (error) {
-        if (error instanceof AuthError) {
-            switch (error.type) {
-                case "CredentialsSignin":
-                    return "Invalid credentials.";
-                default:
-                    return "Unknown error.";
-            }
-        }
-        throw error;
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Unknown error.';
+      }
     }
+    throw error;
+  }
 }
 
 export async function logout() {
-    await signOut({ redirectTo: "/login", redirect: true });
+  await signOut({ redirectTo: '/login', redirect: true });
 }

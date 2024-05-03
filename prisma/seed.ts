0d508fbaa325ca26d4b7dd1cf4304a07bb1d1d0d
Mohammed Ali <mohaali482@gmail.com>
2024-05-03 09:53:39 +0300

    feat: add new fields for user

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,30 +1,32 @@
-import { PrismaClient } from "@prisma/client"
-import bcrypt from "bcrypt"
+import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcrypt';
+import { randomUUID } from 'crypto';
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 async function main() {
-    console.log("Started creating superuser")
-    await prisma.user.create({
-        data: {
-            first_name: process.env.SUPER_USER_FIRST_NAME!,
-            last_name: process.env.SUPER_USER_LAST_NAME!,
-            username: process.env.SUPER_USER_USERNAME!,
-            password: await bcrypt.hash(
-                process.env.SUPER_USER_PASSWORD!,
-                parseInt(process.env.HASH_ROUNDS!)
-            ),
-        },
-    })
-    console.log("Superuser created succefully")
+  console.log('Started creating superuser');
+  await prisma.user.create({
+    data: {
+      first_name: process.env.SUPER_USER_FIRST_NAME!,
+      last_name: process.env.SUPER_USER_LAST_NAME!,
+      username: process.env.SUPER_USER_USERNAME!,
+      password: await bcrypt.hash(
+        process.env.SUPER_USER_PASSWORD!,
+        parseInt(process.env.HASH_ROUNDS!)
+      ),
+      role: 'SUPER_USER'
+    }
+  });
+  console.log('Superuser created succefully');
 }
 
 main()
-    .then(async () => {
-        await prisma.$disconnect()
-    })
-    .catch(async (e) => {
-        console.error(e)
-        await prisma.$disconnect()
-        process.exit(1)
-    })
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async e => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });

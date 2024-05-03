4132d5f05cdd6c1268e65d35021171bf9c1ac49a
Mohammed Ali <mohaali482@gmail.com>
2024-05-03 10:04:27 +0300

    feat: add a customized breadcrumb component

--- src/components/DefaultBreadcrumb.tsx
+++ src/components/DefaultBreadcrumb.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React from 'react'
+import React, { Fragment } from 'react'
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from './ui/breadcrumb'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
@@ -13,17 +13,17 @@ function DefaultBreadcrumb() {
             <BreadcrumbList>
                 {
                     pathNames.map((link, index) =>
-                        <>
+                        <Fragment key={index}>
                             <BreadcrumbItem>
                                 {index + 1 == pathNames.length ?
                                     <BreadcrumbPage>{link[0].toUpperCase() + link.slice(1)}</BreadcrumbPage> :
                                     <BreadcrumbLink asChild>
-                                        <Link href={`/${pathNames.slice(0, index + 1).join('/')}`}>{link[0].toUpperCase() + link.slice(1)}</Link>
+                                        <Link className='text-primary' href={`/${pathNames.slice(0, index + 1).join('/')}`}>{link[0].toUpperCase() + link.slice(1)}</Link>
                                     </BreadcrumbLink>
                                 }
                             </BreadcrumbItem>
                             {index + 1 < pathNames.length ? <BreadcrumbSeparator /> : null}
-                        </>
+                        </Fragment>
                     )}
             </BreadcrumbList>
         </Breadcrumb>

85ff8a116139d235d364675a046eccaf6c6225bc
Mohammed Ali <mohaali482@gmail.com>
2024-05-05 17:25:42 +0300

    feat: add email to environment variable list

--- environment.d.ts
+++ environment.d.ts
@@ -1,15 +1,16 @@
 declare global {
-    namespace NodeJS {
-        interface ProcessEnv {
-            AUTH_SECRET: string;
+  namespace NodeJS {
+    interface ProcessEnv {
+      AUTH_SECRET: string;
 
-            SUPER_USER_USERNAME: string;
-            SUPER_USER_FIRST_NAME: string;
-            SUPER_USER_LAST_NAME: string;
-            SUPER_USER_PASSWORD: string;
-            HASH_ROUNDS: string;
-        }
+      SUPER_USER_USERNAME: string;
+      SUPER_USER_EMAIL: string;
+      SUPER_USER_FIRST_NAME: string;
+      SUPER_USER_LAST_NAME: string;
+      SUPER_USER_PASSWORD: string;
+      HASH_ROUNDS: string;
     }
+  }
 }
 
 export {};

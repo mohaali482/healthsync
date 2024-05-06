3db487d3b8b514e2a0f47b55dd6cf7589f406bc1
Mohammed Ali <mohaali482@gmail.com>
2024-05-06 18:35:13 +0300

    feat: add new schemas

--- src/lib/validations/auth.ts
+++ src/lib/validations/auth.ts
@@ -1,6 +1,16 @@
-import * as z from "zod"
+import * as z from 'zod';
 
 export const userAuthSchema = z.object({
-    username: z.string().min(1, "Username cannot be empty"),
-    password: z.string().min(4, "Password cannot be less than 4 character(s)"),
-})
+  username: z.string().min(1, 'Username cannot be empty'),
+  password: z.string().min(4, 'Password cannot be less than 4 character(s)')
+});
+
+export const changePasswordSchema = z
+  .object({
+    password: z.string().min(1, 'Password is required'),
+    confirm_password: z.string().min(1, 'Confirm password is required')
+  })
+  .refine(data => data.password === data.confirm_password, {
+    message: 'Passwords must match.',
+    path: ['confirm_password']
+  });
--- src/lib/validations/hospital.ts
+++ src/lib/validations/hospital.ts
@@ -21,3 +21,18 @@ export const hospitalAdminSchema = z.object({
   email: z.string().email(),
   password: z.string().min(1, 'Password is required')
 });
+
+export const hospitalAdminEditSchema = z.object({
+  first_name: z.string().min(1, 'First name is required'),
+  last_name: z.string().min(1, 'Last name is required'),
+  username: z.string().min(1, 'Username is required'),
+  email: z.string().email()
+});
+
+export const hospitalEditSchema = z.object({
+  name: z.string().min(1, 'Name is required'),
+  region: z.string().min(1, 'Region is required'),
+  city: z.string().min(1, 'City is required'),
+  zone: z.string().min(1, 'Zone is required'),
+  woreda: z.string().min(1, 'Woreda is required')
+});

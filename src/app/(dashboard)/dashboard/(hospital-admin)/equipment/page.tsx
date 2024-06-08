5cd2f9a54c1bae73d8550b4828a81ac36aec8ead
Mohammed Ali <mohaali482@gmail.com>
2024-06-08 06:56:07 +0300

    fix: change the way to load data of equipments page

--- src/app/(dashboard)/dashboard/(hospital-admin)/equipment/page.tsx
+++ src/app/(dashboard)/dashboard/(hospital-admin)/equipment/page.tsx
@@ -3,15 +3,16 @@ import Table from "./table";
 import { auth } from "@/lib/auth";
 import { redirect } from "next/navigation";
 import { getAllMedicalEquipments } from "@/data/equipment-types";
+import { logout } from "@/app/lib/actions";
 
 export default async function Page() {
     const session = await auth()
-    if (session === null || session.user.role !== "HOSPITAL_ADMIN") {
-        return redirect('/login?from=/dashboard/equipment')
+    if (session === null || session.user.role !== "HOSPITAL_ADMIN" || session.user.hospitalId === null) {
+        return await logout()
     }
 
     const data = await Promise.all([
-        getAllMedicalEquipmentStore(session.user.hospitalId!!),
+        getAllMedicalEquipmentStore(session.user.hospitalId),
         getAllMedicalEquipments()
     ])
 

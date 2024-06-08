7986e62139b2ebf184889f3f118807aac80ba167
Mohammed Ali <68723236+mohaali482@users.noreply.github.com>
2024-06-08 09:24:32 +0300

    Delete prisma/migrations directory

--- prisma/migrations/20240502054543_/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropIndex
-DROP INDEX "User_id_key";
-
--- AlterTable
-ALTER TABLE "User" ADD CONSTRAINT "User_pkey" PRIMARY KEY ("id");
--- prisma/migrations/20240510062152_/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "MedicalEquipmentStore" ADD COLUMN     "thresholdLevel" INTEGER NOT NULL DEFAULT 0;

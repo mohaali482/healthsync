b2e1bfe9c9371fd913f13aa4bffdc7e7b3037b69
Mohammed Ali <mohaali482@gmail.com>
2024-06-08 14:02:50 +0300

    fix: add validator for date to accept string

--- src/lib/validations/reports.ts
+++ src/lib/validations/reports.ts
@@ -4,5 +4,5 @@ export const reportsForm = z.object({
   diseaseId: z.number().min(1, { message: 'This field is required' }),
   confirmedCase: z.number().min(1, { message: 'This field is required' }),
   confirmedDeath: z.number().min(0, { message: 'This field is required' }),
-  reportDate: z.date()
+  reportDate: z.coerce.date()
 });

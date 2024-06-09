8059b8f889a3c7a0327e118ef2c4e55aa0a16f59
Mohammed Ali <mohaali482@gmail.com>
2024-06-09 11:10:15 +0300

    feat: finish implementing change password page

---
+++ src/app/(auth)/change-password/page.tsx
@@ -0,0 +1,37 @@
+import { Metadata } from "next"
+
+import { Icons } from "@/components/icons"
+import { UserChangePasswordForm } from "@/components/forms/UserChangePasswordForm"
+import { auth } from "@/lib/auth"
+import { redirect } from "next/navigation"
+
+export const metadata: Metadata = {
+    title: "Login",
+    description: "Login to your account",
+}
+
+export default async function LoginPage() {
+    const session = await auth()
+    if (!session) return redirect("/login")
+
+
+    return (
+        <div className="container flex h-screen w-screen flex-col items-center justify-center">
+            <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+                <div className="flex flex-col space-y-2 text-center">
+                    <Icons.logo className="mx-auto h-6 w-6" />
+                    <h1 className="text-2xl font-semibold tracking-tight">
+                        Welcome back <span className="text-primary">{session.user.name}</span>
+                    </h1>
+                    <h1 className="text-xl font-semibold tracking-tight">
+                        Change Password
+                    </h1>
+                    <p className="text-sm text-muted-foreground">
+                        Enter your current and new password.
+                    </p>
+                </div>
+                <UserChangePasswordForm />
+            </div>
+        </div>
+    )
+}

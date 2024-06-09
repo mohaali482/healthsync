7eee25f5c30bd01895aff8039f81fc9e311a1c24
Mohammed Ali <mohaali482@gmail.com>
2024-06-09 22:43:29 +0300

    feat: implemented the user role on the header

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -2,12 +2,26 @@ import Layout from "@/components/Layout"
 import { getLinks } from "@/components/navbar-links"
 import { auth } from "@/lib/auth"
 
+const getRoleName = (role: string) => {
+    switch (role) {
+        case "SUPER_USER":
+            return "Super User"
+        case "DATA_ENCODER":
+            return "Data Encoder"
+        default:
+            return "Hospital Admin"
+    }
+}
+
 async function layout({ children }: { children: React.ReactNode }) {
     const session = await auth()
-    const links = getLinks(session?.user.role as any)
+    if (!session) {
+        return
+    }
+    const links = getLinks(session.user.role as any)
 
     return (
-        <Layout links={links}>
+        <Layout role={getRoleName(session.user.role)} links={links}>
             {children}
         </Layout>
     )
